c7c6e59b46ee1ef1bec586ce892977012c03d427
DouglasOlv <douglas4030@gmail.com>
2020-07-05 21:22:34 -0300

    add user finder by email

--- index.ts
+++ index.ts
@@ -1,14 +1,34 @@
-import { ApolloServer, gql } from "apollo-server";
+import { ApolloServer, gql, IResolvers } from "apollo-server";
+import users from "./users";
 
 const typeDefs = gql`
   type Query {
     hello: String
+    users: [User!]!
+    getUserByEmail(email: String!): User!
+  }
+  type User {
+    _id: ID!
+    name: String!
+    email: String!
+    active: Boolean!
+  }
+
+  type Post {
+    _id: ID!
+    title: String!
+    content: String!
+    author: User!
   }
 `;
 
-const resolvers = {
+const resolvers: IResolvers = {
   Query: {
     hello: () => "Hello world",
+    users: () => users,
+    getUserByEmail: (_, args) => {
+      return users.find((user) => user.email === args.email);
+    },
   },
 };
 
